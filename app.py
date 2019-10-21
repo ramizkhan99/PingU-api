182429dfd74a228cb05774731c5ff3ed5a560c19
Ramiz Khan <ramiz.cop@gmail.com>
2019-10-21 18:18:49 +0530

    Might work now

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify, current_app, send_from_directory, send_file
+from flask import Flask, request, jsonify, send_file
 import numpy as np
 import pickle, csv, os, json
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/pingu"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/pingu"
+# mongo = PyMongo(app)
 model = pickle.load(open('model.pickle', 'rb'))
 
 # Routes
@@ -25,22 +25,15 @@ def predict():
 #     return jsonify({'Status': 'Location Captured'})
 
 
-@app.route('/download', methods=['GET'])
-def download_csv():
-    uploads = os.path.join(os.getcwd(), 'test.csv')
-    return send_file(uploads, as_attachment=True)
+# @app.route('/download', methods=['GET'])
+# def download_csv():
+#     download = os.path.join(os.getcwd(), 'test.csv')
+#     return send_file(download, as_attachment=True)
 
 
 @app.route('/generate', methods=['POST'])
 def json_to_csv():
-    # with open('sample.json', 'r') as f:
-    # locations = json.load(f)
     locations = request.get_json(force=True)
-    # return locations
-    # locations = f.read()
-    # locations = str(locations)
-    # locations = locations[:-2] + locations[-1:]
-    # locations = json.loads(locations)
     csv_file = csv.writer(open('test.csv', 'w', newline=''))
     csv_file.writerow(["carrier",
                        "speed",
@@ -52,7 +45,9 @@ def json_to_csv():
                            location['speed'],
                            location['lat'],
                            location['lon']])
-    return "done"
+    # return "done"
+    download = os.path.join(os.getcwd(), 'test.csv')
+    return send_file(download, as_attachment=True)
 
 
 if __name__ == '__main__':
